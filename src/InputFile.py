b28bbebcd19685f293976e73f181442dffc89383
dethaa <sharonmarbun12@gmail.com>
2021-04-05 18:26:41 +0700

    update inputFile

--- src/InputFile.py
+++ src/InputFile.py
@@ -7,24 +7,28 @@ lines = f.readlines()
 #menyimpan banyaknya simpul ke dalam numNode
 numNode = int(lines[0])
 
-#menyimpan data nama simpul ke dalam tabNama dan koordinat ke dalam tabKoor
-tabNama = ["*" for i in range(numNode)]
-tabKoor = [[0.0 for j in range(2)] for i in range(numNode)]
+#menyimpan data nama simpul ke dalam tabNama 
+tabNama = ['*' for i in range(numNode)]
 for i in range(1,numNode+1):
+    tabNama[i-1] = lines[i]
+    tabNama[i-1]=tabNama[i-1].replace('\n','')
+print(tabNama)
+
+#menyimpan data koordinat ke dalam tabKoor
+tabKoor = [[0.0 for j in range(2)] for i in range(numNode)]
+for i in range(numNode+1,numNode*2+1):
     words = lines[i].split()
-    tabNama[i-1] = words[0]
-    tabKoor[i-1][0] = float(words[1])
-    tabKoor[i-1][1] = float(words[2])
-#print(tabNama)
-#print(tabKoor)
+    tabKoor[i-numNode-1][0] = float(words[0])
+    tabKoor[i-numNode-1][1] = float(words[1])
+print(tabKoor)
 
 #menyimpan data matriks ketetanggaan ke dalam tabAdj
 tabAdj = [[False for j in range(numNode)] for i in range(numNode)]
-for i in range(numNode+1, numNode*2+1):
+for i in range(numNode*2+1, numNode*3+1):
     numbers = lines[i].split()
     for j in range(numNode):
         if numbers[j] == "1":
-            tabAdj[i-numNode-1][j] = True
+            tabAdj[i-numNode*2-1][j] = True
 #print(tabAdj)
 
 #mendapatkan nama dari simpul sesuai dengan indeksnya
@@ -39,9 +43,11 @@ def findIdx(name):
 
 #mendapatkan jarak euclidean antara simpul start dan simpul goal
 def getEuclideanDistance(startIdx,goalIdx):
-    diffX = abs(tabKoor[goalIdx][0] - tabKoor[startIdx][0])
-    diffY = abs(tabKoor[goalIdx][1] - tabKoor[startIdx][1])
-    return((diffX**2 + diffY**2)**0.5)
+    diffX = abs(tabKoor[int(goalIdx)][0] - tabKoor[int(startIdx)][0])
+    diffY = abs(tabKoor[int(goalIdx)][1] - tabKoor[int(startIdx)][1])
+    result = (diffX**2 + diffY**2)**0.5
+    #print("jarak dari " + str(startIdx) + " ke " + str(goalIdx) + " = " + str(result))
+    return(result)
 
 #mengisi tabHn dengan nilai heuristik yang dihitung berdasarkan jarak simpul ke simpul goal
 def generateHn(goalIdx, tabHn):
@@ -78,3 +84,28 @@ def isVisited(node, checkIdx):
             if idxs[i] == str(checkIdx):
                 return True
         return False
+
+#mendapatkan nilai g(n) dari simpul node
+def countGn(node):
+    if node.track == "null":
+        return 0
+    else:
+        idxs = node.track.split()
+        sum = getEuclideanDistance(node.idx, idxs[len(idxs)-1])
+        for i in range(len(idxs)-1,0,-1):
+            sum+=getEuclideanDistance(idxs[i],idxs[i-1])
+        return sum
+
+'''
+node = Node(4)
+addParent(node,3)
+addParent(node,2)
+addParent(node,0)
+print(node.track)
+gn = countGn(node)
+print("g(n) = " + str(gn))            
+'''
+
+
+
+
